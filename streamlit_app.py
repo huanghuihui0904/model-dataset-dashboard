a088a2ecaadf6d369de0c69d8a4a008ec6eb1a29
Huihui Huang <71327352+huanghuihui0904@users.noreply.github.com>
2024-09-11 06:20:31 +0000

    only show the dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,13 @@ import streamlit as st
 # Show app title and description.
 st.set_page_config(page_title="Support tickets", page_icon="🎫")
 st.title("🎫 Support tickets")
-st.write(
-    """
-    This app shows how you can build an internal tool in Streamlit. Here, we are 
-    implementing a support ticket workflow. The user can create a ticket, edit 
-    existing tickets, and view some statistics.
-    """
-)
+# st.write(
+#     """
+#     This app shows how you can build an internal tool in Streamlit. Here, we are 
+#     implementing a support ticket workflow. The user can create a ticket, edit 
+#     existing tickets, and view some statistics.
+#     """
+# )
 
 # Create a random Pandas dataframe with existing tickets.
 if "df" not in st.session_state:
@@ -70,32 +70,32 @@ if "df" not in st.session_state:
 
 # We're adding tickets via an `st.form` and some input widgets. If widgets are used
 # in a form, the app will only rerun once the submit button is pressed.
-with st.form("add_ticket_form"):
-    issue = st.text_area("Describe the issue")
-    priority = st.selectbox("Priority", ["High", "Medium", "Low"])
-    submitted = st.form_submit_button("Submit")
+# with st.form("add_ticket_form"):
+#     issue = st.text_area("Describe the issue")
+#     priority = st.selectbox("Priority", ["High", "Medium", "Low"])
+#     submitted = st.form_submit_button("Submit")
 
-if submitted:
-    # Make a dataframe for the new ticket and append it to the dataframe in session
-    # state.
-    recent_ticket_number = int(max(st.session_state.df.ID).split("-")[1])
-    today = datetime.datetime.now().strftime("%m-%d-%Y")
-    df_new = pd.DataFrame(
-        [
-            {
-                "ID": f"TICKET-{recent_ticket_number+1}",
-                "Issue": issue,
-                "Status": "Open",
-                "Priority": priority,
-                "Date Submitted": today,
-            }
-        ]
-    )
+# if submitted:
+#     # Make a dataframe for the new ticket and append it to the dataframe in session
+#     # state.
+#     recent_ticket_number = int(max(st.session_state.df.ID).split("-")[1])
+#     today = datetime.datetime.now().strftime("%m-%d-%Y")
+#     df_new = pd.DataFrame(
+#         [
+#             {
+#                 "ID": f"TICKET-{recent_ticket_number+1}",
+#                 "Issue": issue,
+#                 "Status": "Open",
+#                 "Priority": priority,
+#                 "Date Submitted": today,
+#             }
+#         ]
+#     )
 
-    # Show a little success message.
-    st.write("Ticket submitted! Here are the ticket details:")
-    st.dataframe(df_new, use_container_width=True, hide_index=True)
-    st.session_state.df = pd.concat([df_new, st.session_state.df], axis=0)
+#     # Show a little success message.
+#     st.write("Ticket submitted! Here are the ticket details:")
+#     st.dataframe(df_new, use_container_width=True, hide_index=True)
+#     st.session_state.df = pd.concat([df_new, st.session_state.df], axis=0)
 
 # Show section to view and edit existing tickets in a table.
 st.header("Existing tickets")
@@ -132,41 +132,41 @@ edited_df = st.data_editor(
 )
 
 # Show some metrics and charts about the ticket.
-st.header("Statistics")
+# st.header("Statistics")
 
 # Show metrics side by side using `st.columns` and `st.metric`.
-col1, col2, col3 = st.columns(3)
-num_open_tickets = len(st.session_state.df[st.session_state.df.Status == "Open"])
-col1.metric(label="Number of open tickets", value=num_open_tickets, delta=10)
-col2.metric(label="First response time (hours)", value=5.2, delta=-1.5)
-col3.metric(label="Average resolution time (hours)", value=16, delta=2)
+# col1, col2, col3 = st.columns(3)
+# num_open_tickets = len(st.session_state.df[st.session_state.df.Status == "Open"])
+# col1.metric(label="Number of open tickets", value=num_open_tickets, delta=10)
+# col2.metric(label="First response time (hours)", value=5.2, delta=-1.5)
+# col3.metric(label="Average resolution time (hours)", value=16, delta=2)
 
 # Show two Altair charts using `st.altair_chart`.
-st.write("")
-st.write("##### Ticket status per month")
-status_plot = (
-    alt.Chart(edited_df)
-    .mark_bar()
-    .encode(
-        x="month(Date Submitted):O",
-        y="count():Q",
-        xOffset="Status:N",
-        color="Status:N",
-    )
-    .configure_legend(
-        orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
-    )
-)
-st.altair_chart(status_plot, use_container_width=True, theme="streamlit")
+# st.write("")
+# st.write("##### Ticket status per month")
+# status_plot = (
+#     alt.Chart(edited_df)
+#     .mark_bar()
+#     .encode(
+#         x="month(Date Submitted):O",
+#         y="count():Q",
+#         xOffset="Status:N",
+#         color="Status:N",
+#     )
+#     .configure_legend(
+#         orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
+#     )
+# )
+# st.altair_chart(status_plot, use_container_width=True, theme="streamlit")
 
-st.write("##### Current ticket priorities")
-priority_plot = (
-    alt.Chart(edited_df)
-    .mark_arc()
-    .encode(theta="count():Q", color="Priority:N")
-    .properties(height=300)
-    .configure_legend(
-        orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
-    )
-)
-st.altair_chart(priority_plot, use_container_width=True, theme="streamlit")
+# st.write("##### Current ticket priorities")
+# priority_plot = (
+#     alt.Chart(edited_df)
+#     .mark_arc()
+#     .encode(theta="count():Q", color="Priority:N")
+#     .properties(height=300)
+#     .configure_legend(
+#         orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
+#     )
+# )
+# st.altair_chart(priority_plot, use_container_width=True, theme="streamlit")
